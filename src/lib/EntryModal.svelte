c6e6a40aa2eef528315549085fac5c9dd373852a
Evan-Lim <evanlim804@gmail.com>
2021-12-12 09:25:36 +0000

    until part 9

--- src/lib/EntryModal.svelte
+++ src/lib/EntryModal.svelte
@@ -8,6 +8,32 @@
         best: '😁'
 
     }
+    let day = 1;
+    let month = 1;
+    let year = 2021;
+    let mood = "Okay";
+    let comment = "This is a comment";
+    
+import supabase from '$lib/db';
+
+// Insert entry
+async function saveEntry() {
+    const { error } = await supabase.from('moodEntries').insert(
+   	 {
+   		 user_id: supabase.auth.user().id,
+   		 day: day,
+   		 month: month,
+   		 year: year,
+   		 mood: mood,
+   		 comment: comment
+   	 }
+    );
+    if (error) alert(error.message);
+
+    location.reload(); // Refresh the page.
+}
+
+
 </script>
 <div class="modal fade" id="newEntry" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
@@ -24,19 +50,19 @@
 <div class="row">
     <div class="col">
    	 <form class="form-floating">
-   		 <input type="number" class="form-control" id="dayInput" value="1" min="1" max="31" />
+   		 <input type="number" class="form-control" id="dayInput" bind:value={day} min="1" max="31" />
    		 <label for="dayInput">Day</label>
    	 </form>
     </div>
     <div class="col">
    	 <form class="form-floating">
-   		 <input type="number" class="form-control" id="monthInput" value="1" min="1" max="12"/>
+   		 <input type="number" class="form-control" id="monthInput" bind:value={month} min="1" max="12"/>
    		 <label for="monthInput">Month</label>
    	 </form>
     </div>
     <div class="col">
    	 <form class="form-floating">
-   		 <input type="number" class="form-control" id="yearInput" value="2021" min="2021"/>
+   		 <input type="number" class="form-control" id="yearInput" bind:value={year} min="2021"/>
    		 <label for="yearInput">Year</label>
    	 </form>
     </div>
@@ -59,6 +85,7 @@ id="worst"
 autocomplete="off"
 on:click={() => {
     emoji = emojiList.worst;
+    mood = 'Worst';
 }}
 />
 <label class="btn btn-outline-danger" for="worst">Worst</label>
@@ -74,6 +101,7 @@ id="bad"
 autocomplete="off"
 on:click={() => {
     emoji = emojiList.bad;
+    mood = 'Bad';
 }}
 />
 <label class="btn btn-outline-warning" for="bad">Bad</label>
@@ -89,6 +117,7 @@ id="okay"
 autocomplete="off"
 on:click={() => {
     emoji = emojiList.okay;
+    mood = 'Okay';
 }}
 />
 <label class="btn btn-outline-primary" for="okay">Okay</label>
@@ -104,6 +133,7 @@ id="good"
 autocomplete="off"
 on:click={() => {
     emoji = emojiList.good;
+    mood = 'Good';
 }}
 />
 <label class="btn btn-outline-info" for="good">Good</label>
@@ -119,6 +149,7 @@ id="best"
 autocomplete="off"
 on:click={() => {
     emoji = emojiList.best;
+    mood = 'Best';
 }}
 />
 <label class="btn btn-outline-success" for="best">Best</label>
@@ -128,7 +159,7 @@ on:click={() => {
 </div>
 <!-- Comments -->
 <div class="form-floating">
-    <textarea class="form-control" id="comment" />
+    <textarea class="form-control" id="comment" bind:value={comment}/>
     <label for="commentTextarea">Comments</label>
 </div>
 
@@ -140,7 +171,7 @@ on:click={() => {
 
    		 <div class="modal-footer">
    			 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-   			 <button type="button" class="btn btn-primary">Add</button>
+   			 <button type="button" class="btn btn-primary" on:click={saveEntry}>Add</button>
    		 </div>
    	 </div>
     </div>
