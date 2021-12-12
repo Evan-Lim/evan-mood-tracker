e54155982cc0a273ea527e330a24b7b9bf46db9a
Evan-Lim <evanlim804@gmail.com>
2021-12-12 10:13:08 +0000

    changed something

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,64 +1,60 @@
 <script>
-import Navbar from '$lib/Navbar.svelte';
-import Greeting from '$lib/Greeting.svelte';
-import Entry from '$lib/Entry.svelte';
-import EntryModal from '$lib/EntryModal.svelte';
-import supabase from '$lib/db';
-// Select entries
-async function getEntries() {
-    const { data, error } = await supabase.from('moodEntries').select();
-    if (error) alert(error.message);
+	import Greeting from '$lib/Greeting.svelte';
+	import Entry from '$lib/Entry.svelte';
+	import EntryModal from '$lib/EntryModal.svelte';
+	import supabase from '$lib/db';
+	// Select entries
+	async function getEntries() {
+		const { data, error } = await supabase.from('moodEntries').select();
+		if (error) alert(error.message);
 
-    return data;
-}
-    async function signOut() {
-   	 const { error } = await supabase.auth.signOut();
+		return data;
+	}
+	async function signOut() {
+		const { error } = await supabase.auth.signOut();
 
-   	 if (error) alert(error.message); // alert if error
-    }
-
+		if (error) alert(error.message); // alert if error
+	}
 </script>
-<Navbar pageCurrent={"Home"} />
-<slot />
+
 <Greeting />
 <EntryModal />
 <!-- Entries -->
 <section class="container px-4 py-3">
-    <div class="d-flex justify-content-between">
-   	 <div class="p-2">Mood Log</div>
-   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" data-bs-toggle="modal" data-bs-target="#newEntry"/>
-    </div>
+	<div class="d-flex justify-content-between">
+		<div class="p-2">Mood Log</div>
+		<input
+			class="btn btn-light mb-2"
+			type="button"
+			value="+ New Entry"
+			data-bs-toggle="modal"
+			data-bs-target="#newEntry"
+		/>
+	</div>
 
-    <div class="list-group mb-3">
-   	 <!-- Individual Entries -->
-    	<!-- Individual Entries -->
-{#await getEntries()}
-<p>Fetching data...</p>
-{:then data}
-{#each data as entry}
-    <Entry
-        date={entry.day + '-' + entry.month + '-' + entry.year}
-        mood={entry.mood}
-        comment={entry.comment}
-    />
-{/each}
-{:catch error}
-<p>Something went wrong while fetching the data:</p>
-<pre>{error}</pre>
-{/await}
-
-
-   	 
-    </div>
+	<div class="list-group mb-3">
+		<!-- Individual Entries -->
+		<!-- Individual Entries -->
+		{#await getEntries()}
+			<p>Fetching data...</p>
+		{:then data}
+			{#each data as entry}
+				<Entry
+					date={entry.day + '-' + entry.month + '-' + entry.year}
+					mood={entry.mood}
+					comment={entry.comment}
+				/>
+			{/each}
+		{:catch error}
+			<p>Something went wrong while fetching the data:</p>
+			<pre>{error}</pre>
+		{/await}
+	</div>
 </section>
 
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
-    <button class="btn btn-secondary" on:click={signOut}>Logout</button>
+	<button class="btn btn-secondary" on:click={signOut}>Logout</button>
 </section>
 
 
-
-<style>
-
-</style>
